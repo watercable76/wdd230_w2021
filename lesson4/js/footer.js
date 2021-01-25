87efb87a69217a38eaac5a814399c8cb80c1a939
Nicholas Cable <cab17001@byui.edu>
2021-01-24 21:32:20 -0700

    Changed some styling

--- lesson4/js/footer.js
+++ lesson4/js/footer.js
@@ -1,10 +1,74 @@
 function current_date() {
-    var year = new Date().getFullYear();
-    var output = '&#169; ' + year;
+    var d = new Date();
+    var date = d.getDate();
+    var year = d.getFullYear();
+    var month = d.getMonth();
+    var day = d.getDay();
 
-    var f_ln = document.getElementById('copyright');
-    f_ln.innerHTML = output + f_ln.innerHTML;
+    // check the month and set to the string value
+    switch(month) {
+        case 0:
+            month = 'January';
+            break;
+        case 1:
+            month = 'February';
+            break;
+        case 2:
+            month = 'March';
+            break;
+        case 3:
+            month = 'April';
+            break;
+        case 4:
+            month = 'May';
+            break;
+        case 5:
+            month = 'June';
+            break;
+        case 6:
+            month = 'July';
+            break;
+        case 7:
+            month = 'August';
+            break;
+        case 8:
+            month = 'September';
+            break;
+        case 9:
+            month = 'October';
+            break;
+        case 10:
+            month = 'November';
+            break;
+        case 11:
+            month = 'December';
+            break;
+    }
 
-    var today = new Date().toLocaleDateString()
-    document.getElementById('cur_date').innerHTML = today;
+    // check and set day as the string value
+    switch(day) {
+        case 0:
+            day = 'Sunday';
+            break;
+        case 1:
+            day = 'Monday';
+            break;
+        case 2:
+            day = 'Tuesday';
+            break;
+        case 3:
+            day = 'Wednesday';
+            break;
+        case 4:
+            day = 'Thursday';
+            break;
+        case 5:
+            day = 'Friday';
+            break;
+        case 6:
+            day = 'Saturday';
+            break;
+    }
+
+    document.getElementById('cur_date').innerHTML = `${day}, ${date} ${month} ${year}`;
 }
