f53f1950b58d5c99fe7b85774654c392368b5a3a
Nicholas Cable <cab17001@byui.edu>
2021-03-03 10:16:25 -0700

    populated page with info from another page

---
+++ lesson9/js/townapi.js
@@ -0,0 +1,35 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject); // temporary checking for valid response and data parsing
+
+        const towns = jsonObject['towns'];
+
+        for (let i = 0; i < prophets.length; i++) {
+            let card = document.createElement('section');
+            // create h2 for names, p for birthdate/place of birth, set img to source found
+            let h2 = document.createElement('h2');
+            let p1 = document.createElement('p');
+            let p2 = document.createElement('p');
+            let img = document.createElement('img');
+            let name = prophets[i].name + ' ' + prophets[i].lastname;
+
+            h2.textContent = name;
+            p1.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+            p2.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+            img.setAttribute('src', prophets[i].imageurl);
+            img.setAttribute('alt', 'Image of the prophet ' + name);
+
+            card.appendChild(h2);
+            card.appendChild(p1);
+            card.appendChild(p2);
+            card.appendChild(img);
+
+            document.querySelector('div.cards').appendChild(card);
+        }
+    });
