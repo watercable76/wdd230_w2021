93cd1739f2dc7ead2e2efbcdb819680bbe4efc0e
Nicholas Cable <cab17001@byui.edu>
2021-03-11 12:49:29 -0700

    removed my url to get data

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -40,7 +40,7 @@
  }
  
  
- const ApiURL = `http://api.openweathermap.org/data/2.5/forecast?id=${city_id}&appid=07edcaccc064119c281855eedd314fc9&units=imperial`;
+ const ApiURL = '';
  
  
  function wind_chill(temp, wind) {
