ada7f2f8ce157b09b094347f59a2e038994b2847
Nicholas Cable <cab17001@byui.edu>
2021-03-17 21:41:33 -0600

    imgs aligned, box shadow works.

--- lesson11/js/display.js
+++ lesson11/js/display.js
@@ -32,18 +32,6 @@ images.forEach(image => {
     imgObserver.observe(image);
 });
 
-// function to set picture to col and row start/end
-document.addEventListener("DOMContentLoaded", function set_grid() {
-    var px = document.documentElement.clientWidth;
-    // 16 px to 1 em
-    var em = px / 16;
-    console.log(em);
-
-    if (em > 32.5) {
-        return;
-    }
-});
-
 
 // adjusting the rating for the slider values
 function adjustRating(severity) {
