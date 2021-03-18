940fb07d55d7bdc9d10972afa8244580882b9ae3
Nicholas Cable <cab17001@byui.edu>
2021-03-17 21:47:09 -0600

    fixed winchill formula - forgot logic

--- lesson11/js/weatherapi.js
+++ lesson11/js/weatherapi.js
@@ -82,7 +82,11 @@ const weatherURL = `https://api.openweathermap.org/data/2.5/weather?id=${city_id
 
 
 function wind_chill(temp, wind) {
-    return (35.74 + 0.6125 * temp - (35.75 * wind ** 0.16) + 0.4275 * temp * wind ** 0.16).toFixed(0);
+    if (temp <= 50 && wind > 3) {
+        return 'N/A';
+    } else {
+        return (35.74 + 0.6125 * temp - (35.75 * wind ** 0.16) + 0.4275 * temp * wind ** 0.16).toFixed(0);
+    }
 }
 
 
